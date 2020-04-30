0ebd35404b7465ad321a4ea1080ee767a360f1fa
Vinod S <s.vinod52@gmail.com>
2020-04-30 16:18:05 +0530

    styling

--- dist/script.js
+++ dist/script.js
@@ -1,19 +1,19 @@
 const canvas = document.getElementById('canvas');
 const ctx = canvas.getContext('2d');
 
-let x=0;
+let x = 0;
 let degrees = 0;
 let img = new Image();
 let fileName = '';
-let rotate=false;
+let rotate = false;
 let wrh = 0;
 let newWidth = 0;
 let newHeight = 0;
 let cropper = '';
 let caman;
 let url;
-let i=0;
-let j=0;
+let i = 0;
+let j = 0;
 
 const downloadBtn = document.getElementById('download-btn');
 const uploadFile = document.getElementById('upload-file');
@@ -21,97 +21,97 @@ const revertBtn = document.getElementById('revert-btn');
 
 
 
-$(document).on('change', 'input[type=range]', function() {
-    var bright = parseInt($('#bright').val());
-    var cntrst = parseInt($('#contrast').val());
-    var saturation = parseInt($('#saturation').val());
-      caman.revert();
-      caman.brightness(bright).contrast(cntrst).saturation(saturation);
-  caman.render(function() {
-      cropper.replace(this.toBase64(), true);
-    });
+$(document).on('change', 'input[type=range]', function () {
+  var bright = parseInt($('#bright').val());
+  var cntrst = parseInt($('#contrast').val());
+  var saturation = parseInt($('#saturation').val());
+  caman.revert();
+  caman.brightness(bright).contrast(cntrst).saturation(saturation);
+  caman.render(function () {
+    cropper.replace(this.toBase64(), true);
   });
+});
 
 
 
 
-uploadFile.addEventListener('change', (e)=>{
-  
+uploadFile.addEventListener('change', (e) => {
+
   const file = document.getElementById('upload-file').files[0];
   console.log(file)
   const reader = new FileReader();
-  
-  if(file){
+
+  if (file) {
     fileName = file.name;
     reader.readAsDataURL(file);
   }
-  
-  reader.addEventListener('load',()=>{
-      url = URL.createObjectURL(file);
+
+  reader.addEventListener('load', () => {
+    url = URL.createObjectURL(file);
     console.log(url)
-    caman = Caman(canvas, url, function() {
+    caman = Caman(canvas, url, function () {
 
-                        //alert('file url: ' + url);
+      //alert('file url: ' + url);
 
-                        URL.revokeObjectURL(url);
-                        cropper = new Cropper(canvas,{
+      URL.revokeObjectURL(url);
+      cropper = new Cropper(canvas, {
 
-                          viewMode: 0,
-                          background: false,
-                          rotatable: true,
-                        });
-                    });
-   var uploadedImageURL = URL.createObjectURL(file);
+        viewMode: 0,
+        background: false,
+        rotatable: true,
+      });
+    });
+    var uploadedImageURL = URL.createObjectURL(file);
     console.log(canvas)
-                   // canvas.destroy().getAttribute('src', uploadedImageURL).options();
-                    
+    // canvas.destroy().getAttribute('src', uploadedImageURL).options();
+
   })
 });
 
 document.addEventListener("click", e => {
   if (e.target.classList.contains("filter-btn")) {
-      if (e.target.classList.contains("vintage-add")) {
- 
-        caman.revert();
-        caman.vintage().render();
-      }
+    if (e.target.classList.contains("vintage-add")) {
+
+      caman.revert();
+      caman.vintage().render();
+    }
     else if (e.target.classList.contains("lomo-add")) {
 
-        caman.revert();
-        caman.lomo().render();
+      caman.revert();
+      caman.lomo().render();
 
     } else if (e.target.classList.contains("clarity-add")) {
-      
-        caman.revert();
-        caman.clarity().render();
-    
+
+      caman.revert();
+      caman.clarity().render();
+
     } else if (e.target.classList.contains("sincity-add")) {
-        caman.revert();
-        caman.sinCity().render();
+      caman.revert();
+      caman.sinCity().render();
 
     } else if (e.target.classList.contains("crossprocess-add")) {
-    
-        caman.revert();
-        caman.crossProcess().render();
-    
+
+      caman.revert();
+      caman.crossProcess().render();
+
     } else if (e.target.classList.contains("pinhole-add")) {
-      
-        caman.revert();
-        caman.pinhole().render();
-      
+
+      caman.revert();
+      caman.pinhole().render();
+
     } else if (e.target.classList.contains("nostalgia-add")) {
-     
-        caman.revert();
-       caman.nostalgia().render();
-     
+
+      caman.revert();
+      caman.nostalgia().render();
+
     } else if (e.target.classList.contains("hermajesty-add")) {
-     
-        caman.revert();
-        caman.herMajesty().render();     
-    }else if (e.target.classList.contains("revert-btn")){
-       caman.revert();
+
+      caman.revert();
+      caman.herMajesty().render();
+    } else if (e.target.classList.contains("revert-btn")) {
+      caman.revert();
     }
-     caman.render(function() {
+    caman.render(function () {
       cropper.replace(this.toBase64(), true);
     });
   }
@@ -119,9 +119,9 @@ document.addEventListener("click", e => {
 
 
 revertBtn.addEventListener("click", e => {
-        caman.revert();
-    
-  
+  caman.revert();
+
+
 });
 
 downloadBtn.addEventListener("click", () => {
@@ -145,41 +145,41 @@ function download(canvas, filename) {
 
 
 
-document.getElementById('rotate').addEventListener("click",()=>{
-  caman.render(function() {
+document.getElementById('rotate').addEventListener("click", () => {
+  caman.render(function () {
     cropper.rotate(90);
-      cropper.replace(this.toBase64(), true);
-    });
-  
-  
+    cropper.replace(this.toBase64(), true);
+  });
+
+
 });
 
-document.getElementById('flipV').addEventListener("click",()=>{
-  i+=1
-  caman.render(function() {
-    if(i%2==0){
+document.getElementById('flipV').addEventListener("click", () => {
+  i += 1
+  caman.render(function () {
+    if (i % 2 == 0) {
       cropper.scale(1, 1);
     }
-    else{
-    cropper.scale(1, -1);
+    else {
+      cropper.scale(1, -1);
     }
-      cropper.replace(this.toBase64(), true);
-    });
-  
-  
+    cropper.replace(this.toBase64(), true);
+  });
+
+
 });
 
-document.getElementById('flipH').addEventListener("click",()=>{
-  j+=1
-  caman.render(function() {
-    if(j%2==0){
+document.getElementById('flipH').addEventListener("click", () => {
+  j += 1
+  caman.render(function () {
+    if (j % 2 == 0) {
       cropper.scale(1, 1);
     }
-    else{
-    cropper.scale(-1, 1);
+    else {
+      cropper.scale(-1, 1);
     }
-      cropper.replace(this.toBase64(), true);
-    });
-  
-  
+    cropper.replace(this.toBase64(), true);
+  });
+
+
 });
